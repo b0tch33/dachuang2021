f0284fefa18e8e846f95ddab205cc77bdef163cb
b0tch33 <45527382+b0tch33@users.noreply.github.com>
2021-01-29 16:10:23 +0800

    Update chuli.py

--- chuli.py
+++ chuli.py
@@ -9,9 +9,11 @@ Created on Tue Jan 26 15:00:27 2021
 
 import cv2
 import numpy as np
+from sklearn.decomposition import PCA
 
-path = "E:/study/dachuang/data_test/1/10071_1961-04-03_2010.jpg"
+path = "E:/study/dachuang/data_test/10/43088_1925-05-12_1956.jpg"
 im = cv2.imread(path)
+print(im.shape)
 face_model = cv2.CascadeClassifier(r'D:/anaconda3/Lib/site-packages/cv2/data/haarcascade_frontalface_alt.xml')
 # 图片进行灰度处理
 im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
@@ -26,18 +28,24 @@ for (x,y,w,h) in faces:
     face_im = im[y:y+h, x:x+w]
 
 # 将裁剪人脸缩放为255*255像素大小
-width, height = [255, 255]
+width, height = [64, 64]
 dim = (width, height)
 # 进行缩放
 face_im_255 = cv2.resize(face_im, dim, interpolation=cv2.INTER_AREA)
+
+pca = PCA(n_components=1)
+t = face_im_255.reshape(-1,1)
+pca.fit(face_im_255.reshape(-1,1))
+face_im_255_new = pca.transform(t)
+print(face_im_255_new)
 
 # 将图片（这里等于矩阵）存放进data列表，准备进行下一步
 data = []
 data.append(face_im_255)
 print(data[0])
 
-# 显示图片
-cv2.imshow('image', im)
-cv2.imshow('face', face_im_255)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# # 显示图片
+# cv2.imshow('image', im)
+# cv2.imshow('face', face_im_255)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
