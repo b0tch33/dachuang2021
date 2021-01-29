41f02c8112c709f88f6aab5a2c950d9c5db78b0e
b0tch33 <45527382+b0tch33@users.noreply.github.com>
2021-01-29 15:23:38 +0800

    Update chuli.py

--- chuli.py
+++ chuli.py
@@ -14,9 +14,9 @@ path = "E:/study/dachuang/data_test/1/10071_1961-04-03_2010.jpg"
 im = cv2.imread(path)
 face_model = cv2.CascadeClassifier(r'D:/anaconda3/Lib/site-packages/cv2/data/haarcascade_frontalface_alt.xml')
 # 图片进行灰度处理
-gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
 # 人脸检测
-faces = face_model.detectMultiScale(gray)
+faces = face_model.detectMultiScale(im)
 # print(faces)
 # 裁剪人脸
 for (x,y,w,h) in faces:
@@ -34,7 +34,7 @@ face_im_255 = cv2.resize(face_im, dim, interpolation=cv2.INTER_AREA)
 # 将图片（这里等于矩阵）存放进data列表，准备进行下一步
 data = []
 data.append(face_im_255)
-print(data)
+print(data[0])
 
 # 显示图片
 cv2.imshow('image', im)
