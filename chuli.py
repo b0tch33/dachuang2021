1f188557d2a133d4bffe624a318aaee3e8b35d35
b0tch33 <45527382+b0tch33@users.noreply.github.com>
2021-02-04 14:49:08 +0800

    降维好像成功了

--- chuli.py
+++ chuli.py
@@ -15,21 +15,6 @@ from sklearn.decomposition import PCA
 width, height = [400, 400]
 dim = (width, height)
 
-def tezhenglian(num):
-    new_image = face_im_255_new[:,num]
-    # new_image = new_image.astype(np.uint8)
-    new_image = scale(new_image) * 255
-    new_image = new_image.astype(np.uint8)
-    print(new_image)
-    new_image = new_image.reshape(20,20)
-    new_image = cv2.resize(new_image, dim, interpolation=cv2.INTER_AREA)
-    # print(type(new_image))
-    return new_image
-
-# 归一化
-def scale(A):
-    return (A-np.min(A))/(np.max(A) - np.min(A))
-
 # 这里用读取所有图片的代码取代之
 path = "E:/study/dachuang/data_test/10/43088_1925-05-12_1956.jpg"
 im = cv2.imread(path)
@@ -41,45 +26,31 @@ face_model = cv2.CascadeClassifier(r'D:/anaconda3/Lib/site-packages/cv2/data/haa
 im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
 # 人脸检测
 faces = face_model.detectMultiScale(im)
-# print(faces)
 # 裁剪人脸
 for (x,y,w,h) in faces:
-    #1.原始图片；2坐标点；3.矩形宽高 4.颜色值(RGB)；5.线框
-    # cv2.rectangle(im, (x,y), (x+w,y+h), (0,255,0), 2)
-    # print(x,y,w,h)
     face_im = im[y:y+h, x:x+w]
 
 
 # 进行缩放
-face_im_255 = cv2.resize(face_im, dim, interpolation=cv2.INTER_AREA)
+face_im = cv2.resize(face_im, dim, interpolation=cv2.INTER_AREA)
 
 # PCA降维（未完成）
-pca = PCA(n_components=100)
-face_im_255_new = pca.fit_transform(face_im_255)
-# print(face_im_255_new.shape)
-# new_image = np.around(face_im_255_new)
-temp_im = np.zeros((400,400), dtype=np.uint8)
-temp_im += 255
-# print(temp_im.shape)
-for i in range(100):
-    temp_im = temp_im + tezhenglian(i)
-    print(i)
-    cv2.imshow('face_new', temp_im)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-print("finish")
+pca = PCA(n_components=20)
+face_im_new = pca.fit_transform(face_im)
+huanyuan = pca.inverse_transform(face_im_new)
+huanyuan = huanyuan.astype(np.uint8)
+print(huanyuan)
+print(pca.explained_variance_ratio_)
 # print(new_image)
 
 # 将图片（这里等于矩阵）存放进data列表，准备进行下一步
 data = []
-# data.append(new_image)
+data.append(huanyuan)
 # print(data[0])
 
 # # 显示图片
-# cv2.imshow('image', im)
-# cv2.imshow('face', face_im_255)
-# cv2.imshow('face_0', new_image)
-zuhe_im = face_im_255 + temp_im
-cv2.imshow('face_new', zuhe_im)
+cv2.imshow('image', im)
+cv2.imshow('face', face_im)
+cv2.imshow('face_new', huanyuan)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
