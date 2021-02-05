26dbbb102c4e0826f156b67785ecfcfda639ca4d
b0tch33 <45527382+b0tch33@users.noreply.github.com>
2021-02-05 15:51:27 +0800

    yuchuli.py需要分批跑
    第一批跑data_test的1到5,第二批是6-10,依此类推,需要跑21次,才能将所有降维后的图片跑出来

--- yuchuli.py
+++ yuchuli.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Thu Feb  4 16:59:25 2021
+
+@author: 66441
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Thu Feb  4 14:50:12 2021
 
 @author: 66441
@@ -9,38 +16,70 @@ from PIL import Image
 import cv2
 import os
 import warnings
-warnings.filterwarnings("error", category=UserWarning)
+warnings.filterwarnings("ignore")
 
 import _pickle as cPickle
 import numpy as np
 import threading
 import time
 import copy
+import uuid
 from sklearn.decomposition import PCA
 
 time_start=time.time()
 
 path = "E:/study/dachuang/data_test"
+path_jiangwei = "E:/study/dachuang/data_jiangwei"
 images = []
 datas = []
 
-width, height = [400, 400]
+width, height = [128, 128]
 dim = (width, height)
 
 # 读取图片线程
 def read_img(path, i):
+    global v
+    v = 1
     path1 = path + "/" + str(i)
     for image in os.listdir(path1):
         im = Image.open(path1 + '/' + image)
-        # im.close()
-        images.append(im)
-        print(len(images))
+        face_model = cv2.CascadeClassifier(r'D:/anaconda3/Lib/site-packages/cv2/data/haarcascade_frontalface_alt.xml')
+        # 图片进行灰度处理
+        im_gray = im.convert("L")
+        im_gray = np.array(im_gray)
+        face_im = copy.deepcopy(im_gray)
+        # 人脸检测
+        faces = face_model.detectMultiScale(im_gray)
+        if faces == ():
+            continue
+        # 裁剪人脸
+        for (x,y,w,h) in faces:
+            face_im = im_gray[y:y+h, x:x+w]
+        
+        # 进行缩放
+        face_im = cv2.resize(face_im, dim, interpolation=cv2.INTER_AREA)
+        
+        # PCA降维
+        pca = PCA(n_components=8)
+        face_im_new = pca.fit_transform(face_im)
+        huanyuan = pca.inverse_transform(face_im_new)
+        huanyuan = huanyuan.astype(np.uint8)
+        # print(huanyuan)
+        # print(pca.explained_variance_ratio_)
+        # print(new_image)
+        
+        # 将每一张降维后的图片（矩阵）存为jpg文件，（这个之后做）5000个一组依次存进以1为起始的文件夹内
+        uuid_str = uuid.uuid4().hex
+        name = path_jiangwei + '/' + uuid_str + ".jpg"
+        cv2.imwrite(name, huanyuan)
+        print("图片数量：", v)
+        v += 1
         im.close()
         
 # 创建线程
 threads = []
 x = 1
-for t in range(0, 105):
+for t in range(0, 5):
     t = threading.Thread(target=read_img, args=(path, x))
     threads.append(t)
     x += 1
@@ -64,52 +103,13 @@ for t in range(0, 105):
 # images.show()
 # print("图片数量：", images)
 
-# 主函数，调用线程
+# 主函数，调用线程预处理
 if __name__=="__main__":
+    v = 1
     for thr in threads:
         thr.start()
-    for i in range(10):
+    for i in range(3):
         threads[i].join()
-    
-    print("图片数量：", len(images))
-    
-    for i in range(100):
-        print(images[i])
         
     time_end=time.time()
-    print('totally cost',time_end-time_start)
-    
-    v = 1
-    
-    for im in images:
-        print(v)
-        im_t = np.array(copy.deepcopy(im))
-        if v < 5:
-            print(im_t)
-        # im = cv2.cvtColor(numpy.asarray(Img_img))
-        face_model = cv2.CascadeClassifier(r'D:/anaconda3/Lib/site-packages/cv2/data/haarcascade_frontalface_alt.xml')
-        # 图片进行灰度处理
-        im_gray = cv2.cvtColor(im_t, cv2.COLOR_RGB2GRAY)
-        # 人脸检测
-        faces = face_model.detectMultiScale(im_gray)
-        # 裁剪人脸
-        for (x,y,w,h) in faces:
-            face_im = im_gray[y:y+h, x:x+w]
-        
-        
-        # 进行缩放
-        face_im = cv2.resize(face_im, dim, interpolation=cv2.INTER_AREA)
-        
-        # PCA降维（未完成）
-        pca = PCA(n_components=20)
-        face_im_new = pca.fit_transform(face_im)
-        huanyuan = pca.inverse_transform(face_im_new)
-        huanyuan = huanyuan.astype(np.uint8)
-        # print(huanyuan)
-        # print(pca.explained_variance_ratio_)
-        # print(new_image)
-        
-        # 将图片（这里等于矩阵）存放进data列表，准备进行下一步
-        datas.append(huanyuan)
-        v += 1
-    print("图片数量：", len(datas))
+print('totally cost',time_end-time_start)
