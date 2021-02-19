2a94daa59e5bbea23dbbf55d83d8764ec948932c
b0tch33 <45527382+b0tch33@users.noreply.github.com>
2021-02-19 16:09:04 +0800

    实际上是knn

--- julei.py
+++ julei.py
@@ -7,35 +7,47 @@ Created on Sat Feb  6 14:19:19 2021
 
 import cv2
 import os
+import random
 import numpy as np
 from PIL import Image
+from sklearn import neighbors 
 from sklearn.cluster import KMeans
 import shutil
 
-path = "E:/study/dachuang/data_jiangwei111"
+path = "E:/study/dachuang/data_jiangwei_sex"
 
 train = []
 test = []
+labels = []
 images = os.listdir(path)
 
 for image in os.listdir(path):
+    label = image[0]
+    # labels.append(label)
     im = Image.open(path + '/' + image)
     im = np.array(im)
     im = im.flatten()
-    if len(train) <= 1600:
-        train.append(im)
-    else:
-        test.append(im)
+    train.append([im, label])
 
-# print(train)
-kmeans = KMeans(n_clusters=2)
-kmeans.fit(train) 
-print(kmeans.labels_)
+for i in range(600):
+    randIndex = int(np.random.uniform(0, len(train)))
+    test.append(train[randIndex])
+    del(train[randIndex])
+# print(test)
+
+# # print(labels)
+knn = neighbors.KNeighborsClassifier(algorithm='kd_tree', n_neighbors=2)
+knn.fit([i[0] for i in train], [i[1] for i in train]) 
+# print(kmeans.labels_)
 # print(images[0])
-for idx in range(len(kmeans.labels_)):
-    oldname = path + '/' + images[idx]
-    newname = 'E:/study/dachuang/' + str(kmeans.labels_[idx]) + '/' + images[idx]
-    shutil.copyfile(oldname, newname)
+# for idx in range(len(kmeans.labels_)):
+#     oldname = path + '/' + images[idx]
+#     newname = 'E:/study/dachuang/' + str(kmeans.labels_[idx]) + '/' + images[idx]
+#     shutil.copyfile(oldname, newname)
     
-# res = kmeans.predict(test) 
+res = knn.predict([i[0] for i in test]) 
 # print(res)
+# print([i[1] for i in test])
+error_num = np.sum(res != [i[1] for i in test]) #统计分类错误的数目
+num = len(test) #测试集的数目
+print("Total num:",num," Wrong num:", error_num,"  准确率:",(num - error_num) / float(num))
