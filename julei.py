cc44dd212fb5bb9b60d76ef27e1dc00a12cccc9e
b0tch33 <45527382+b0tch33@users.noreply.github.com>
2021-02-21 15:03:29 +0800

    差标签传递，使用已有少量标签推测剩余图片标签

--- julei.py
+++ julei.py
@@ -12,9 +12,11 @@ import numpy as np
 from PIL import Image
 from sklearn import neighbors 
 from sklearn.cluster import KMeans
+from sklearn.semi_supervised import LabelPropagation
 import shutil
 
 path = "E:/study/dachuang/data_jiangwei_sex"
+path_1000 = "E:/study/dachuang/data_jiangwei"
 
 train = []
 test = []
@@ -29,12 +31,12 @@ for image in os.listdir(path):
     im = im.flatten()
     train.append([im, label])
 
+# KNN
 for i in range(600):
     randIndex = int(np.random.uniform(0, len(train)))
     test.append(train[randIndex])
     del(train[randIndex])
 # print(test)
-
 # # print(labels)
 knn = neighbors.KNeighborsClassifier(algorithm='kd_tree', n_neighbors=2)
 knn.fit([i[0] for i in train], [i[1] for i in train]) 
@@ -51,3 +53,8 @@ res = knn.predict([i[0] for i in test])
 error_num = np.sum(res != [i[1] for i in test]) #统计分类错误的数目
 num = len(test) #测试集的数目
 print("Total num:",num," Wrong num:", error_num,"  准确率:",(num - error_num) / float(num))
+
+# LP（标签传递）
+
+
+
